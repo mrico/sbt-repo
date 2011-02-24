9768b98535ad7cbb062e90dca99126959b3c64ff
Marco Rico-Gomez <m.ricogomez@gmail.com>
2011-02-24 16:23:10 +0100

    initial commit

--- project/build/RepoProject.scala
+++ project/build/RepoProject.scala
@@ -1,7 +1,9 @@
 import sbt._
 
-class RepoProject(info: ProjectInfo) extends ProcessorProject(info) {
-
+class RepoProject(info: ProjectInfo) extends ProcessorProject(info) {  
+  override def managedStyle = ManagedStyle.Maven  
+  lazy val publishTo = Resolver.file("GitHub Pages", new java.io.File("../mrico.github.com/maven/"))
+  
   val nexusIndexer = "org.sonatype.nexus" % "nexus-indexer" % "3.0.4"
   val dispatch = "net.databinder" %% "dispatch-http" % "0.7.8"
 }
